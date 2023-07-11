b5f13be7ed10e8000f091be7146ad4347f49b433
byungyong <quddyddlrk@naver.com>
2023-07-11 22:14:20 +0900

    검사결과 추출/ 전후질문이동 버튼

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,112 +8,195 @@ import Button from './Button';
 
 function Card(props) {
     const [index,setIndex] = useState(0);
-    const [result,setResult] = useState();
-
-    const question_list = [
-        'question1',
-        'question2',
-        'question3',
-        'question4',
-        'question5',
-        'question6',
-        'question7',
-        'question8',
-        'question9',
-        'question10',
-        'question11',
-        'question12',
-        'question13',
-        'question14',
-        'question15',
-    ];
-
-    const answer_list = [
-        {
-            answer1: '가1',
-            answer2: '나1'
-        },
-        {
-            answer1: '가2',
-            answer2: '나2'
-        },
-        {
-            answer1: '가3',
-            answer2: '나3'
-        },
-        {
-            answer1: '가4',
-            answer2: '나4'
-        },
-        {
-            answer1: '가5',
-            answer2: '나5'
-        },
-        {
-            answer1: '가6',
-            answer2: '나6'
-        },
-        {
-            answer1: '가7',
-            answer2: '나7'
-        },
-        {
-            answer1: '가8',
-            answer2: '나8'
-        },
-        {
-            answer1: '가9',
-            answer2: '나9'
-        },
-        {
-            answer1: '가10',
-            answer2: '나10'
-        },
-        {
-            answer1: '가11',
-            answer2: '나11'
-        },
-        {
-            answer1: '가12',
-            answer2: '나12'
-        },
-        {
-            answer1: '가13',
-            answer2: '나13'
-        },
-        {
-            answer1: '가14',
-            answer2: '나14'
-        },
-        {
-            answer1: '가15',
-            answer2: '나15'
-        },
-    ];
-
-
-    const [question,setQuestion] = useState(question_list[index]);
+    const [result,setResult] = useState([]);
     
-    function clickHandler() {
-        if (index <= 14) {
-            setIndex(prev => prev + 1)
-            setQuestion(question_list[index]);
+    const content_list = [
+        {
+            question : 1, 
+            answer1 : ['가1', 'E'],
+            answer2 : ['나1', 'I'],
+        },
+        {
+            question : 2,
+            answer1: ['가2', 'E'],
+            answer2: ['나2', 'I'],
+        },
+        {
+            question : 3,
+            answer1: ['가3', 'E'],
+            answer2: ['나3', 'I'],
+        },
+        {
+            question : 4,
+            answer1: ['가4', 'S'],
+            answer2: ['나5', 'N'],
+        },
+        {
+            question : 5,
+            answer1: ['가5', 'S'],
+            answer2: ['나5', 'N'],
+        },
+        {
+            question : 6,
+            answer1: ['가6', 'S'],
+            answer2: ['나6', 'N'],
+        },
+        {
+            question : 7,
+            answer1: ['가7', 'T'],
+            answer2: ['나7', 'F'],
+        },
+        {
+            question : 8,
+            answer1: ['가8', 'T'],
+            answer2: ['나8', 'F'],
+        },
+        {
+            question : 9,
+            answer1: ['가9', 'T'],
+            answer2: ['나9', 'F'],
+        },
+        {
+            question : 10,
+            answer1: ['가10', 'P'],
+            answer2: ['나10', 'J'],
+        },
+        {
+            question : 11,
+            answer1: ['가11', 'P'],
+            answer2: ['나11', 'J'],
+        },
+        {
+            question : 12,
+            answer1: ['가12', 'P'],
+            answer2: ['나12', 'J'],
+        },
+        {
+            question : 13,
+            answer1: ['가13', 'A'],
+            answer2: ['나13', 'T'],
+        },
+        {
+            question : 14,
+            answer1: ['가14', 'A'],
+            answer2: ['나14', 'T'],
+        },
+        {
+            question : 15,
+            answer1: ['가15', 'A'],
+            answer2: ['나15', 'T'],
+        },
+    ];
+
+    function viewResult() {console.log(result);} //test
+    
+    function resultSetter(){
+        let iScore = 0;
+        let nScore = 0;
+        let fScore = 0;
+        let jScore = 0;
+        let aScore = 0;
+        const MBTI = []
+
+        for (let i in result) {
+            if (result[i] === 'I') {
+                iScore ++;
+            }
+            if (result[i] === 'N') {
+                nScore ++;
+            }
+            if (result[i] === 'F') {
+                fScore ++;
+            }
+            if (result[i] === 'J') {
+                jScore ++;
+            }
+            if (result[i] === 'A') {
+                aScore ++;
+            }
+        }
+
+        if (iScore >= 2) {
+            MBTI.push('I')
         } else {
-            alert('결과페이지 출력');
+            MBTI.push('E')
+        }
+
+        if (nScore >= 2) {
+            MBTI.push('N')
+        } else {
+            MBTI.push('S')
+        }
+
+        if (fScore >= 2) {
+            MBTI.push('F')
+        } else {
+            MBTI.push('T')
+        }
+
+        if (jScore >= 2) {
+            MBTI.push('J')
+        } else {
+            MBTI.push('P')
+        }
+
+        if (aScore >= 2) {
+            MBTI.push('A')
+        } else {
+            MBTI.push('T')
+        }
+
+        console.log('당신의 MBTI',MBTI)
+
+        return MBTI;
+    }
+
+    function answerClickHandler(e) {                        // 답변 클릭 시
+        if (index < 14) {                                  // 다음 질문 있으면 다음질문 렌더링
+            setIndex(prev => prev + 1);                      
+            setResult(prev => [...prev,e.target.children[0].textContent]);
+        } else {                                           // 마지막 질문이면 결과 페이지로 이동
+            setResult(prev => [...prev,e.target.children[0].textContent]);
+            resultSetter()
+            alert('');
+        }
+        console.log(result)
+    }
+
+    function viewPrevQuestionHandler() {                      // 이전 질문으로 가는 함수
+        if( index > 0 && index <= 14){
+            setIndex(prev => prev-1);
+            setResult(prev => [...prev].splice(0,index-1))
+        } else {
+            alert('첫질문')
+        }
+    }
+    
+    function viewNextQuestionHandler() {                      // 다음 질문으로 가는 함수
+        if( index < 14){
+            setIndex(prev => prev + 1);
+        } else {
+            alert('마지막질문');
         }
     }
 
     return (
         <div>
-            <progress value={index+1} max={15}></progress>
-            <div className={classes.card_container}>
-                <p className={classes.question_content}>
-                    {question}
-                </p>
+            <progress value={index+1} max={15} className={classes.progress_bar}></progress>
+            <div className={classes.content_box}>
+                <button className={classes.left_arrow} onClick={viewPrevQuestionHandler}>⇦</button>
+                <div className={classes.card_container}>{content_list[index].question}</div>
+                <button className={classes.right_arrow} onClick={viewResult}>⇨</button>
             </div>
             <div className={classes.answer_button}>
-            <Button onclick={clickHandler}>{answer_list[index].answer1}</Button>
-            <Button onclick={clickHandler}>{answer_list[index].answer2}</Button>
+                <Button onclick={answerClickHandler}>
+                    {content_list[index].answer1[0]}
+                    <div hidden>{content_list[index].answer1[1]}</div>
+                </Button>
+                <Button onclick={answerClickHandler}>
+                    {content_list[index].answer2[0]}
+                    <div hidden>{content_list[index].answer2[1]}</div>
+                </Button>
             </div>
         </div>
     );
